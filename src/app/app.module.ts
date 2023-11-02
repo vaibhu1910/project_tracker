e54d98acacaebe5e9704fe98c28637684fb0bd91
dhrithi <dhriti280@gmail.com>
2023-11-02 12:54:38 -0700

    added view issue page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,11 @@ import { DragDropModule } from '@angular/cdk/drag-drop';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { TaskTrackerComponent } from './task-tracker/task-tracker.component';
 import { TaskComponent } from './task/task.component';
+import { CKEditorModule } from '@ckeditor/ckeditor5-angular';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatListModule } from '@angular/material/list';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [
@@ -26,6 +31,11 @@ import { TaskComponent } from './task/task.component';
     DragDropModule,
     BrowserAnimationsModule,
     BrowserModule,
+    CKEditorModule,
+    MatTabsModule,
+    MatListModule,
+    MatDividerModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/task/task.component.ts
+++ src/app/task/task.component.ts
@@ -1,11 +1,64 @@
-import { Component, Input } from '@angular/core';
-import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
+import { Component, Input } from "@angular/core";
+import {
+  CdkDragDrop,
+  moveItemInArray,
+  transferArrayItem,
+} from "@angular/cdk/drag-drop";
+import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
+
+const editorConfig = {
+  // ui: 'pt',
+  // language: 'pt',
+  toolbar: {
+    items: [
+      "undo",
+      "redo",
+      "|",
+      "heading",
+      "|",
+      "bold",
+      "italic",
+      "link",
+      "|",
+      "strike",
+      "code",
+      "|", // Não tem ainda
+      "bulletedList",
+      "numberedList",
+    ],
+  },
+};
 
 @Component({
-  selector: 'app-task',
-  templateUrl: './task.component.html',
-  styleUrls: ['./task.component.css']
+  selector: "app-task",
+  templateUrl: "./task.component.html",
+  styleUrls: ["./task.component.css"],
 })
 export class TaskComponent {
-  @Input() content:any;
+  @Input() content: any;
+
+  ckeditor = ClassicEditor;
+  editorConfig = editorConfig;
+
+  simpleItems: string[] = [
+    "Task 1 name changed",
+    "Task 2 completed require additional fucntions",
+    "Project name changed",
+  ];
+  // fileToUpload: File | null = null;
+
+  // handleFileInput(files: FileList) {
+  //   this.fileToUpload = files.item(0);
+  // }
+  uploadedFileName: string | null = null;
+
+  handleFileInput(event: any): void {
+    const file: File = event.target.files[0];
+    
+    if (file) {
+      // Do something with the file, e.g., upload it to a server
+      this.uploadedFileName = file.name;
+      console.log('Uploaded file:', file.name);
+    }
+  }
 }
